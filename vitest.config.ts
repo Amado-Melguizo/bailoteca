1f898e11189916d5eb52bc6e58285482e8964751
Amado Melguizo <amado.melgizo@gmail.com>
2026-08-03 19:44:00 +0200

    Feature: update vue config with github actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import vue from "@vitejs/plugin-vue";
 
 export default mergeConfig(
   viteConfig,
@@ -10,5 +11,8 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
+    plugins: [vue],
+
+    base: '/bailoteca/',
   }),
 )
